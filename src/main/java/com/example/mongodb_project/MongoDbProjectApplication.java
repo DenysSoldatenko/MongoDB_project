3047f11a18d764737fb89031409989ae9a8ef26b
DenysSoldatenko <soldatenko911@gmail.com>
2023-07-21 15:45:00 +0300

    Initial commit

---
+++ src/main/java/com/example/mongodb_project/MongoDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodb_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MongoDbProjectApplication.class, args);
+  }
+
+}
