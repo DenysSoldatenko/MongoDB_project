db6ee2980ec721c5ec685cba9e73bed3c28f6c33
DenysSoldatenko <soldatenko911@gmail.com>
2024-03-27 15:30:00 +0300

    Updated the dependency versions

--- src/main/java/com/example/project/configurations/SpringFoxConfig.java
+++ src/main/java/com/example/project/configurations/SpringFoxConfig.java
@@ -1,30 +1,39 @@
 package com.example.project.configurations;
 
-import static springfox.documentation.builders.RequestHandlerSelectors.any;
-import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
-
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * Configuration class for SpringFox Swagger documentation.
  */
 @Configuration
+@OpenAPIDefinition(
+  info = @Info(
+    contact = @Contact(
+      name = "Denys Soldatenko",
+      email = "john.doe@example.com",
+      url = "https://example.com/about"
+    ),
+    title = "MongoDB project API",
+    version = "1.0",
+    description = "MongoDB project API Information",
+    license = @License(
+      name = "Example License",
+      url = "https://example.com"
+    ),
+    termsOfService = "https://example.com/terms"
+  ),
+  servers = {
+    @Server(
+      url = "http://localhost:8080",
+      description = "Local server"
+    )
+  }
+)
 public class SpringFoxConfig {
 
-  /**
-   * Configures the Docket bean for Swagger API documentation.
-   *
-   * @return the Docket bean representing the Swagger documentation configuration
-   */
-  @Bean
-  public Docket api() {
-    return new Docket(SWAGGER_2)
-    .select()
-    .apis(any())
-    .paths(PathSelectors.any())
-    .build();
-  }
 }
